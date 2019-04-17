20a969f008d90979a33787f108a803e7b01498d3
Guilherme S <sguilh@amazon.com>
2019-04-17 10:47:05 +0200

    Updates

---
+++ precache-manifest.58e5b4480674cc1d201db0cff69b5baa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "fd2679dd86fb7fc67334",
+    "url": "/static/js/main.d73c0e44.chunk.js"
+  },
+  {
+    "revision": "8ccab566347453416526",
+    "url": "/static/js/2.57364a4f.chunk.js"
+  },
+  {
+    "revision": "fd2679dd86fb7fc67334",
+    "url": "/static/css/main.4c038e99.chunk.css"
+  },
+  {
+    "revision": "e188d0ba2fc4e555f3faa18bf6b79cab",
+    "url": "/index.html"
+  }
+];
